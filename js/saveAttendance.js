d3b2e6d1c78422a35a55a296f7f07a53787e57c6
Jake Crowley <jakecrow@gmail.com>
2020-04-29 03:00:55 -0400

    Added usage of LocalStorage to save attendance if you leave and rejoin the room

--- js/saveAttendance.js
+++ js/saveAttendance.js
@@ -3,7 +3,7 @@ var saveData = (function () {
     document.body.appendChild(a);
     a.style = "display: none";
     return function (data, fileName) {
-        var blob = new Blob([text], {type: 'text/plain'}),
+        var blob = new Blob([ls.getItem(code + "_text")], {type: 'text/plain'}),
             url = window.URL.createObjectURL(blob);
         a.href = url;
         a.download = fileName;
@@ -15,4 +15,4 @@ var saveData = (function () {
 var date = new Date();
 var dateStr = ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '-' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '-' + date.getFullYear();
 var timeStr = date.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3").replace(":", "-");
-saveData(text, "attendance " + dateStr + " " + timeStr + ".csv");
+saveData(ls.getItem(code + "_text"), "attendance " + dateStr + " " + timeStr + ".csv");
--- js/startAttendance.js
+++ js/startAttendance.js
@@ -1,27 +1,51 @@
-var text = "Name,Event,Time\r\n";
-var attendees = [];
+if(ran == null){
+	var ran = true;
+	
+	var code = document.URL.split("/")[3].split("?")[0];
+	var ls = window.localStorage;
 
-setInterval(function() {
-	var curr = [];
-	var names = document.querySelectorAll('div[data-self-name="You"]');
-	for(var i = 0; i < names.length; i += 2){
-		var name = names[i].innerText;
-		if(name != "" && name != "You"){
-			curr[curr.length] = name;
-		}
+	if(ls.getItem(code + "_text") == null){
+		ls.setItem(code + "_text", "Name,Event,Time\r\n");
 	}
-	
-	var newattendees = curr.filter(d => !attendees.includes(d));
-	for(var i = 0; i < newattendees.length; i++){
-		var name = newattendees[i];
-		text += name + ",Joined," + new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() + "\r\n";
+
+	var attendees = [];
+
+	if(ls.getItem(code + "_json") != null){
+		attendees = JSON.parse(ls.getItem(code + "_json"));
 	}
-	
-	var leftattendees = attendees.filter(d => !curr.includes(d));
-	for(var i = 0; i < leftattendees.length; i++){
-		var name = leftattendees[i];
-		text += name + ",Left," + new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() + "\r\n";
-	}
-	
-	attendees = curr;
-}, 1000);
+
+	var checkExist = setInterval(function() {
+	   if (document.querySelector('#lcsclient') != null) {
+			clearInterval(checkExist);
+			checkInterval = setInterval(function() {
+				var curr = [];
+				var names = document.querySelectorAll('div[data-self-name="You"]');
+				for(var i = 0; i < names.length; i += 2){
+					var name = names[i].innerText;
+					if(name != "" && name != "You" && !name.startsWith("Presentation (")){
+						curr[curr.length] = name;
+					}
+				}
+				
+				var newattendees = curr.filter(d => !attendees.includes(d));
+				for(var i = 0; i < newattendees.length; i++){
+					var name = newattendees[i];
+					var text = ls.getItem(code + "_text");
+					text += name + ",Joined," + new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() + "\r\n";
+					ls.setItem(code + "_text", text);
+				}
+				
+				var leftattendees = attendees.filter(d => !curr.includes(d));
+				for(var i = 0; i < leftattendees.length; i++){
+					var name = leftattendees[i];
+					var text = ls.getItem(code + "_text");
+					text += name + ",Left," + new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() + "\r\n";
+					ls.setItem(code + "_text", text);
+				}
+				
+				attendees = curr;
+				ls.setItem(code + "_json", JSON.stringify(attendees));
+			}, 1000);
+	   }
+	}, 1000);
+}
